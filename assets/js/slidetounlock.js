fd1fcc02e0802a0d0cbbaa3a4bc1e1ee474586b9
vagabond <athomec@pie.com.tw>
2023-03-23 21:23:12 +0800

    tt

--- assets/js/slidetounlock.js
+++ assets/js/slidetounlock.js
@@ -25,19 +25,28 @@ $(function () {
   		}
   	}
   });*/
-  var el = document.querySelector('.js-slidetounlock-slider');
-  var hammertime = new Hammer(el, {});
-  hammertime.on('pan', function (ev) {
-    if (ev.deltaX > 150) {
-      $(".js-slidetounlock-txt").fadeOut(200);
-      setTimeout(function () {
-        $(".offcanvas-bottom").find("button").click();
-        $(".js-slidetounlock-txt").fadeIn(200);
-        el.style.transform = 'translateX(' + 0 + 'px)';
-      }, 500);
-    } else {
-      el.style.transform = 'translateX(' + ev.deltaX + 'px)';
-    }
+  var sliders = document.querySelectorAll('.js-slidetounlock-slider');
+  Array.prototype.forEach.call(sliders, function (slider) {
+    var hammertime = new Hammer(slider, {});
+    hammertime.on('pan', function (ev) {
+      if (ev.deltaX > 150) {
+        var txt = slider.querySelector('.js-slidetounlock-txt');
+        $(txt).fadeOut(200);
+        setTimeout(function () {
+          var offcanvas = slider.closest('.offcanvas-bottom');
+          $(offcanvas).find("button").click();
+          $(txt).fadeIn(200);
+          slider.style.transform = 'translateX(' + 0 + 'px)';
+        }, 500);
+      } else {
+        slider.style.transform = 'translateX(' + ev.deltaX + 'px)';
+      }
+    });
+    hammertime.on('panend', function (ev) {
+      if (ev.deltaX < 150) {
+        slider.style.transform = 'translateX(' + 0 + 'px)';
+      }
+    });
   });
 });
 //# sourceMappingURL=slidetounlock.js.map

d13b71c7efea1467492db2cde14f9a0b6340ac9f
vagabond <athomec@pie.com.tw>
2023-03-24 00:10:27 +0800

    test

--- assets/js/slidetounlock.js
+++ assets/js/slidetounlock.js
@@ -1,37 +1,14 @@
 "use strict";
 
 $(function () {
-  /*$(".js-slidetounlock-slider").draggable({
-  	axis: 'x',
-  	containment: 'parent',
-  	drag: function(event, ui) {
-  		if (ui.position.left > 150) {
-  			$(".js-slidetounlock-txt").fadeOut(200);
-  			setTimeout(function () {
-  				$(".offcanvas-bottom").find("button").click();
-  				$(".js-slidetounlock-txt").fadeIn(200);
-  				$(".js-slidetounlock-slider").css("left","0");
-  			}, 500);
-  		} else {
-  			// Apparently Safari isn't allowing partial opacity on text with background clip? Not sure.
-  			// $("h2 span").css("opacity", 100 - (ui.position.left / 5))
-  		}
-  	},
-  	stop: function(event, ui) {
-  		if (ui.position.left < 151) {
-  			$(this).animate({
-  				left: 0
-  			})
-  		}
-  	}
-  });*/
   var sliders = document.querySelectorAll('.js-slidetounlock-slider');
   Array.prototype.forEach.call(sliders, function (slider) {
     var hammertime = new Hammer(slider, {});
     hammertime.on('pan', function (ev) {
-      if (ev.deltaX > 150) {
-        var txt = slider.querySelector('.js-slidetounlock-txt');
+      if (ev.deltaX > 180) {
+        var txt = slider.nextElementSibling;
         $(txt).fadeOut(200);
+        console.log(txt);
         setTimeout(function () {
           var offcanvas = slider.closest('.offcanvas-bottom');
           $(offcanvas).find("button").click();

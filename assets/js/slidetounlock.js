64720cf560bca864fd917d04c5f9f05a87f4fed8
vagabond <athomec@pie.com.tw>
2023-03-23 16:48:27 +0800

    ee

--- assets/js/slidetounlock.js
+++ assets/js/slidetounlock.js
@@ -1,27 +1,42 @@
 "use strict";
 
 $(function () {
-  $(".js-slidetounlock-slider").draggable({
-    axis: 'x',
-    containment: 'parent',
-    drag: function drag(event, ui) {
-      if (ui.position.left > 150) {
-        $(".js-slidetounlock-txt").fadeOut(200);
-        setTimeout(function () {
-          $(".offcanvas-bottom").find("button").click();
-          $(".js-slidetounlock-txt").fadeIn(200);
-          $(".js-slidetounlock-slider").css("left", "0");
-        }, 500);
-      } else {// Apparently Safari isn't allowing partial opacity on text with background clip? Not sure.
-        // $("h2 span").css("opacity", 100 - (ui.position.left / 5))
-      }
-    },
-    stop: function stop(event, ui) {
-      if (ui.position.left < 151) {
-        $(this).animate({
-          left: 0
-        });
-      }
+  /*$(".js-slidetounlock-slider").draggable({
+  	axis: 'x',
+  	containment: 'parent',
+  	drag: function(event, ui) {
+  		if (ui.position.left > 150) {
+  			$(".js-slidetounlock-txt").fadeOut(200);
+  			setTimeout(function () {
+  				$(".offcanvas-bottom").find("button").click();
+  				$(".js-slidetounlock-txt").fadeIn(200);
+  				$(".js-slidetounlock-slider").css("left","0");
+  			}, 500);
+  		} else {
+  			// Apparently Safari isn't allowing partial opacity on text with background clip? Not sure.
+  			// $("h2 span").css("opacity", 100 - (ui.position.left / 5))
+  		}
+  	},
+  	stop: function(event, ui) {
+  		if (ui.position.left < 151) {
+  			$(this).animate({
+  				left: 0
+  			})
+  		}
+  	}
+  });*/
+  var el = document.querySelector('.js-slidetounlock-slider');
+  var hammertime = new Hammer(el, {});
+  hammertime.on('pan', function (ev) {
+    if (ev.deltaX > 150) {
+      $(".js-slidetounlock-txt").fadeOut(200);
+      setTimeout(function () {
+        $(".offcanvas-bottom").find("button").click();
+        $(".js-slidetounlock-txt").fadeIn(200);
+        el.style.transform = 'translateX(' + 0 + 'px)';
+      }, 500);
+    } else {
+      el.style.transform = 'translateX(' + ev.deltaX + 'px)';
     }
   });
 });
